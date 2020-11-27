505bc6ee7a2232a58fd73878f214c8f223642009
YYuan1414 <yuanyiren19900728@163.com>
2020-11-28 00:22:05 +0800

    feat: add ParkingLotsDto

---
+++ ParkingLotApi/Dtos/ParkingLotsDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkingLotApi.Dtos
+{
+    public class ParkingLotsDto
+    {
+        public ParkingLotsDto()
+        {
+        }
+
+        public ParkingLotsDto(string name, int capacity, string location)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.Location = location;
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public string Location { get; set; }
+
+    }
+}
