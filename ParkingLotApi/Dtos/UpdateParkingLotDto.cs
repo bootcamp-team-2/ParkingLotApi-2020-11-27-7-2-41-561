feb4e6f3ab58e1fab0da3fb2b00989357d6192f0
Lei ZHU <lzhu19@slb.com>
2020-11-29 16:21:36 +0800

    feat:can update parkinglot capacity succefully

---
+++ ParkingLotApi/Dtos/UpdateParkingLotDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ParkingLotApi.Entities;
+
+namespace ParkingLotApi.Dtos
+{
+    public class UpdateParkingLotDto
+    {
+        public UpdateParkingLotDto()
+        {
+        }
+
+        public UpdateParkingLotDto(ParkingLotEntity parkingLotEntity)
+        {
+            this.Capacity = parkingLotEntity.Capacity;
+        }
+
+        public int Capacity { get; set; }
+    }
+}
